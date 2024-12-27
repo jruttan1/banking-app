088c25e1ffd852e787fa03fa1f7c5728f051a9bb
Jack Ruttan <jack@jacks-air-2.home>
2024-12-27 01:40:35 -0500

    implemented initial ui components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,28 @@
+"use client";
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: "Jack"};
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts ={[]}
+            totalBanks={3}
+            totalCurrentBalance={1954.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
